8dd578576495e15d66da1f607936748c8b013d1c
dmitrii89 <dmitry_cher@mail.ru>
2019-01-24 20:45:49 +0300

    table initialization commit

--- chapter23/src/main/java/com/spring/hibernate/TestJdbc.java
+++ chapter23/src/main/java/com/spring/hibernate/TestJdbc.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class TestJdbc {
     public static void main(String[] args) {
-        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=utf8";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=Europe/Moscow&useUnicode=true&characterEncoding=utf8";
         String user = "hbstudent";
         String password = "hbstudent";
         try {
