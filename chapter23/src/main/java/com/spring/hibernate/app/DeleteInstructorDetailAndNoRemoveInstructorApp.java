a1d67e8d02a8107ecc23a5d0c1fe683e95961d59
dmitrii89 <dmitry_cher@mail.ru>
2019-01-28 20:52:55 +0300

    chapter 23 one to one bidirectional remove cases

--- chapter23/src/main/java/com/spring/hibernate/app/DeleteInstructorDetailAndNoRemoveInstructorApp.java
+++ chapter23/src/main/java/com/spring/hibernate/app/DeleteInstructorDetailAndNoRemoveInstructorApp.java
@@ -1,7 +1,8 @@
 package com.spring.hibernate.app;
 
-import com.spring.hibernate.entity.InstructorBiderectional;
+import com.spring.hibernate.entity.InstructorBiderectionalNoRemove;
 import com.spring.hibernate.entity.InstructorDetailBidirectional;
+import com.spring.hibernate.entity.InstructorDetailBidirectionalNoRemove;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,22 +10,26 @@ import org.hibernate.cfg.Configuration;
 /**
  * Created by Dmitrii on 19.01.2019.
  */
-public class BiderectionalOneToOneDemoApp {
+public class DeleteInstructorDetailAndNoRemoveInstructorApp {
     public static void main(String[] args) {
 
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(InstructorBiderectional.class)
-                .addAnnotatedClass(InstructorDetailBidirectional.class)
+                .addAnnotatedClass(InstructorBiderectionalNoRemove.class)
+                .addAnnotatedClass(InstructorDetailBidirectionalNoRemove.class)
                 .buildSessionFactory();
         Session session = factory.getCurrentSession();
 
         try {
             session.beginTransaction();
 
-            InstructorDetailBidirectional detail = session.get(InstructorDetailBidirectional.class, 12);
+            InstructorDetailBidirectionalNoRemove detail = session.get(InstructorDetailBidirectionalNoRemove.class, 3);
             System.out.println("Instructor detail: " + detail);
             System.out.println("Instructor : " + detail.getInstructor());
+
+            System.out.println("Deleting instructor details!!!");
+            detail.getInstructor().setInstructorDetail(null); // we should remove link before deletion
+            session.delete(detail);
 
             session.getTransaction().commit();
             System.out.println("Done!");
@@ -38,7 +43,10 @@ public class BiderectionalOneToOneDemoApp {
     }
 
 //    Hibernate: select instructor0_.id as id1_1_0_, instructor0_.hobby as hobby2_1_0_, instructor0_.youtube_channel as youtube_3_1_0_, instructor1_.id as id1_0_1_, instructor1_.email as email2_0_1_, instructor1_.first_name as first_na3_0_1_, instructor1_.instructor_detail_id as instruct5_0_1_, instructor1_.last_name as last_nam4_0_1_ from instructor_detail instructor0_ left outer join instructor instructor1_ on instructor0_.id=instructor1_.instructor_detail_id where instructor0_.id=?
-//    Instructor detail: InstructorDetailBidirectional{id=1, youtubeChannel='youtube.com/dmch', hobby='Coding, snowboarding, swimming}
-//    Instructor : InstructorBiderectional{id=1, firstName='Dima', lastName='Cherry', email='dmch@mail.ru', instructorDetail=InstructorDetailBidirectional{id=1, youtubeChannel='youtube.com/dmch', hobby='Coding, snowboarding, swimming}}
+//    Instructor detail: InstructorDetailBidirectional{id=3, youtubeChannel='youtube.com/dmch', hobby='Coding, snowboarding, swimming}
+//    Instructor : InstructorBiderectional{id=3, firstName='Dima', lastName='Cherry', email='dmch@mail.ru', instructorDetail=InstructorDetailBidirectional{id=3, youtubeChannel='youtube.com/dmch', hobby='Coding, snowboarding, swimming}}
+//    Deleting instructor details!!!
+//    Hibernate: update instructor set email=?, first_name=?, instructor_detail_id=?, last_name=? where id=?
+//    Hibernate: delete from instructor_detail where id=?
 //    Done!
 }
--- chapter23/src/main/java/com/spring/hibernate/app/DeleteInstructorDetailApp.java
+++ chapter23/src/main/java/com/spring/hibernate/app/DeleteInstructorDetailApp.java
@@ -9,7 +9,7 @@ import org.hibernate.cfg.Configuration;
 /**
  * Created by Dmitrii on 19.01.2019.
  */
-public class BiderectionalOneToOneDemoApp {
+public class DeleteInstructorDetailApp {
     public static void main(String[] args) {
 
         SessionFactory factory = new Configuration()
@@ -22,9 +22,12 @@ public class BiderectionalOneToOneDemoApp {
         try {
             session.beginTransaction();
 
-            InstructorDetailBidirectional detail = session.get(InstructorDetailBidirectional.class, 12);
+            InstructorDetailBidirectional detail = session.get(InstructorDetailBidirectional.class, 2);
             System.out.println("Instructor detail: " + detail);
             System.out.println("Instructor : " + detail.getInstructor());
+
+            System.out.println("Deleting instructor details!!!");
+            session.delete(detail);
 
             session.getTransaction().commit();
             System.out.println("Done!");
@@ -38,7 +41,10 @@ public class BiderectionalOneToOneDemoApp {
     }
 
 //    Hibernate: select instructor0_.id as id1_1_0_, instructor0_.hobby as hobby2_1_0_, instructor0_.youtube_channel as youtube_3_1_0_, instructor1_.id as id1_0_1_, instructor1_.email as email2_0_1_, instructor1_.first_name as first_na3_0_1_, instructor1_.instructor_detail_id as instruct5_0_1_, instructor1_.last_name as last_nam4_0_1_ from instructor_detail instructor0_ left outer join instructor instructor1_ on instructor0_.id=instructor1_.instructor_detail_id where instructor0_.id=?
-//    Instructor detail: InstructorDetailBidirectional{id=1, youtubeChannel='youtube.com/dmch', hobby='Coding, snowboarding, swimming}
-//    Instructor : InstructorBiderectional{id=1, firstName='Dima', lastName='Cherry', email='dmch@mail.ru', instructorDetail=InstructorDetailBidirectional{id=1, youtubeChannel='youtube.com/dmch', hobby='Coding, snowboarding, swimming}}
+//    Instructor detail: InstructorDetailBidirectional{id=2, youtubeChannel='youtube.com/dmch', hobby='Coding, snowboarding, swimming}
+//    Instructor : InstructorBiderectional{id=2, firstName='Dima', lastName='Cherry', email='dmch@mail.ru', instructorDetail=InstructorDetailBidirectional{id=2, youtubeChannel='youtube.com/dmch', hobby='Coding, snowboarding, swimming}}
+//    Deleting instructor details!!!
+//    Hibernate: delete from instructor where id=?
+//    Hibernate: delete from instructor_detail where id=?
 //    Done!
 }
