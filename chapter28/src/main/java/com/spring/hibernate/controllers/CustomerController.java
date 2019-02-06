7ccb014b43a6541abbb3b56b8427b43db2e8bec4
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-02-06 22:34:35 +0300

    chapter 31 add customer

--- chapter28/src/main/java/com/spring/hibernate/controllers/CustomerController.java
+++ chapter28/src/main/java/com/spring/hibernate/controllers/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,5 +26,22 @@ public class CustomerController {
         model.addAttribute("customers", customers);
 
         return "list-customers";
+    }
+
+    @GetMapping(value = "/showFormForAdd")
+    public String addCustomerForm(Model model) {
+
+        Customer customer = new Customer();
+
+        model.addAttribute("customer", customer);
+        return "customer-form";
+    }
+
+    @PostMapping(value = "/saveCustomer")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+
+        customerService.saveCustomer(customer);
+
+        return "redirect:/customer/list";
     }
 }
--- chapter28/src/main/java/com/spring/hibernate/service/CustomerService.java
+++ chapter28/src/main/java/com/spring/hibernate/service/CustomerService.java
@@ -9,4 +9,6 @@ import java.util.List;
  */
 public interface CustomerService {
     List<Customer> getCustomers();
+
+    void saveCustomer(Customer customer);
 }
