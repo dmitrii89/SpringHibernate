a588f4b228cff02130a2ee79e0400989c86d2751
dmitrii89 <dmitry_cher@mail.ru>
2019-02-12 12:24:54 +0300

    chapter 47 spring security with default login form

---
+++ chapter47/src/main/java/com/spring/hibernate/config/SecurityWebApplicatonInitializer.java
@@ -0,0 +1,8 @@
+package com.spring.hibernate.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicatonInitializer extends AbstractSecurityWebApplicationInitializer {
+
+
+}
