4fe210cd516300c21191315b060855d11f859114
dmitrii89 <dmitry_cher@mail.ru>
2019-02-12 21:05:06 +0300

    chapter 49 bootstrap login form

--- chapter48/src/main/java/com/spring/hibernate/controller/LoginController.java
+++ chapter48/src/main/java/com/spring/hibernate/controller/LoginController.java
@@ -2,14 +2,13 @@ package com.spring.hibernate.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/loginPage")
     public String getLoginForm() {
-        return "login";
+        return "fancy-login";
     }
 
 }
