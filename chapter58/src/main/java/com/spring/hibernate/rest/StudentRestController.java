e98effebd4461fbde083bebf3eaff7e887839530
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-02-17 15:27:07 +0300

    chapter 59 exception handling

--- chapter58/src/main/java/com/spring/hibernate/rest/StudentRestController.java
+++ chapter58/src/main/java/com/spring/hibernate/rest/StudentRestController.java
@@ -1,11 +1,18 @@
 package com.spring.hibernate.rest;
 
 import com.spring.hibernate.entity.Student;
+import com.spring.hibernate.entity.StudentErrorResponse;
+import com.spring.hibernate.exceptions.StudentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,12 +21,50 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
+        students.add(new Student("Petya", "Ivanov"));
+        students.add(new Student("Vasya", "Petrov"));
+        students.add(new Student("Misha", "Sidorov"));
+    }
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        Student dima = new Student("Petya", "Ivanov");
-        Student anya = new Student("Vasya", "Petrov");
-        Student mark = new Student("Misha", "Sidorov");
-        return Arrays.asList(dima, anya, mark);
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable Integer studentId) {
+        if(studentId >= students.size() || studentId < 0){
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    //catches all types of exceptions
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
