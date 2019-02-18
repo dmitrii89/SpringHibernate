ddba6a9ceea4c09f38fdaea1d192fe363ddb5b9b
dmitrii89 <dmitry_cher@mail.ru>
2019-02-18 20:47:12 +0300

    chapter 68 add, update, delete customer

--- chapter63/src/main/java/com/spring/hibernate/rest/CustomerRestController.java
+++ chapter63/src/main/java/com/spring/hibernate/rest/CustomerRestController.java
@@ -4,11 +4,9 @@ import com.spring.hibernate.entity.Customer;
 import com.spring.hibernate.exceptions.CustomerNotFoundException;
 import com.spring.hibernate.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -21,6 +19,7 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers(){
         List<Customer> customers = customerService.getCustomers();
+        customers.sort(Comparator.comparingInt(Customer::getId));
         return customers;
     }
 
@@ -33,6 +32,30 @@ public class CustomerRestController {
         }
 
         return customer;
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer){
+        customer.setId(0);
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+
+    @PutMapping("/customers")
+    public Customer updateCustomer(@RequestBody Customer customer){
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+
+    @DeleteMapping("/customers/{customerId}")
+    public String deleteCustomer(@PathVariable int customerId){
+        Customer customer = customerService.getCustomer(customerId);
+        if(customer == null){
+            throw new CustomerNotFoundException("Customer id not found - " + customerId);
+        }
+
+        customerService.deleteCustomer(customerId);
+        return "Deleted customer with id = " + customerId;
     }
 
 }
