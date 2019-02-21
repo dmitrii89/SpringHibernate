06b11f07188e6a3b569c9ab9dc187d7ffba3cc93
dmitrii89 <dmitry_cher@mail.ru>
2019-02-21 21:15:38 +0300

    chapter 77 spring data rest

---
+++ chapter77/src/main/java/com/springboot/hibernate/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.hibernate.dao;
+
+import com.springboot.hibernate.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
